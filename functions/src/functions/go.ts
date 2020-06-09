ea363b2f6c2afe2c3cee0ceb9eabc9ba70f23c13
Felix O <felix.o.stack@gmail.com>
2020-06-09 12:56:05 -0700

    apply works

--- functions/src/functions/go.ts
+++ functions/src/functions/go.ts
@@ -4,6 +4,12 @@ import { docSet } from '../modules/admin';
 export const apply = functions.https.onCall(
 async (p, c) => {
     const user: any = c.auth;
+
+    const uid: string = (user)?user.uid:p.uid;
+
+    if(p.password) delete p.password;
+    if(p.confirm_password) delete p.confirm_password;
+    if(p.uid) delete p.uid;
     
-    return await docSet(`go/${user.uid}`, p);
+    return await docSet(`go/${uid}`, p);
 });
