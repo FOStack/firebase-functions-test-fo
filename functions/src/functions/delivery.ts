599e64a5410dcea8050c5a1a108ad84415b6223c
Felix O <felix.o.stack@gmail.com>
2020-09-16 01:40:34 -0700

    driver sort and assign

--- functions/src/functions/delivery.ts
+++ functions/src/functions/delivery.ts
@@ -21,13 +21,33 @@ export const tasks = functions.runWith({memory: '2GB'}).pubsub
 
     const jobs: Promise<any>[] = [];
     const now = timestamp();
+
     const query = db.collection('deliveries')
                     .where('performAt', '<=', now)
                     .where('status', '==', 'scheduled');
     const queue = await query.get();
+
+    const query2 = db.collection('drivers');
+    const driverRefs = await query2.get();
+    let drivers: any = [];
+
+    driverRefs.forEach(d => {
+        const cid = d.ref.id;
+        drivers.push({
+            cid,
+            orders: 0
+        });
+    });
+    
     queue.forEach(i => {
-        const job = i.ref.update({ status: 'assigned', cid: 'djjd2iu29d2kkj2kmc9' });
+        const l = drivers.reduce((a:any,b:any) => a.count<b.count?a:b);
+        const j = drivers.findIndex((d: any) => d == l);
+        const job = i.ref.update({ 
+            status: 'assigned', 
+            cid: drivers[j].cid
+        });
         jobs.push(job);
+        drivers[j].count += 1;
     });
 
     return await Promise.all(jobs);
